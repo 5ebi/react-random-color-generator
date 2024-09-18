092cdacee96d0cc3fd7c6e950f3beb376c7127f4
Sebastian <sebi.speiser@gmx.net>
2024-09-18 19:05:12 +0200

    i have added some finishing css touches

--- src/App.js
+++ src/App.js
@@ -1,20 +1,80 @@
 import randomColor from 'randomcolor';
-import useState from 'react';
+import { useState } from 'react';
 
+const black = 'black';
+const offwhite = 'whitesmoke';
 export default function App() {
-  const color = randomColor();
+  const [color, setColor] = useState(black);
 
-  console.log(color);
+  const handleGenerate = () => {
+    const newColor = randomColor({ luminosity: 'dark' });
+    setColor(newColor);
+  };
+
   return (
     <>
-      <h1 style={{ fontSize: 100, textAlign: 'center', color: color }}>
-        Random Color Generator
-      </h1>
-      <h2 style={{ fontSize: 50, textAlign: 'center', color: color }}>
-        {color}
-      </h2>
-      {/* <button onClick={sayHello}>Default</button>; */}
+      <div
+        style={{
+          minHeight: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'flex-start',
+          backgroundColor: color,
+        }}
+      >
+        <h1 style={{ fontSize: 100, color: offwhite }}>
+          Random Color Generator
+        </h1>
+
+        <div
+          style={{
+            backgroundColor: 'black',
+            fontSize: '20px',
+            fontWeight: '900',
+            color: '#ffffff',
+            width: '350px',
+            padding: '40px',
+            borderRadius: '10px',
+            textAlign: 'center',
+          }}
+        >
+          <p
+            style={{
+              fontWeight: 800,
+              color: offwhite,
+            }}
+          >
+            THIS IS THE GENERATED COLOR:
+            <br></br>
+          </p>
+          <p
+            style={{
+              fontSize: '35px',
+              color: color,
+            }}
+          >
+            {color}
+          </p>
+
+          <button
+            onClick={handleGenerate}
+            style={{
+              width: '130px',
+              padding: '10px',
+              fontSize: '18px',
+              fontWeight: '900',
+              cursor: 'pointer',
+              backgroundColor: offwhite,
+              color: black,
+              borderRadius: '10px',
+              border: 5,
+            }}
+          >
+            Generate
+          </button>
+        </div>
+      </div>
     </>
   );
 }
-console.log('test');
