1f737b43d7337519abcbbbc0c365cf70c8aaf20c
Sebastian <sebi.speiser@gmx.net>
2024-09-18 19:42:08 +0200

    changed the es lint error

--- src/App.js
+++ src/App.js
@@ -12,69 +12,65 @@ export default function App() {
   };
 
   return (
-    <>
+    <div
+      style={{
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        backgroundColor: color,
+      }}
+    >
+      <h1 style={{ fontSize: 100, color: offwhite }}>Random Color Generator</h1>
+
       <div
         style={{
-          minHeight: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'flex-start',
-          backgroundColor: color,
+          backgroundColor: 'black',
+          fontSize: '20px',
+          fontWeight: '900',
+          color: '#ffffff',
+          width: '350px',
+          padding: '40px',
+          borderRadius: '10px',
+          textAlign: 'center',
         }}
       >
-        <h1 style={{ fontSize: 100, color: offwhite }}>
-          Random Color Generator
-        </h1>
-
-        <div
+        <p
           style={{
-            backgroundColor: 'black',
-            fontSize: '20px',
-            fontWeight: '900',
-            color: '#ffffff',
-            width: '350px',
-            padding: '40px',
-            borderRadius: '10px',
-            textAlign: 'center',
+            fontWeight: 800,
+            color: offwhite,
           }}
         >
-          <p
-            style={{
-              fontWeight: 800,
-              color: offwhite,
-            }}
-          >
-            Generated Color:
-            <br></br>
-          </p>
-          <p
-            style={{
-              fontSize: '35px',
-              color: color,
-            }}
-          >
-            {color}
-          </p>
+          Generated Color:
+          <br></br>
+        </p>
+        <p
+          style={{
+            fontSize: '35px',
+            color: color,
+          }}
+        >
+          {color}
+        </p>
 
-          <button
-            onClick={handleGenerate}
-            style={{
-              width: '130px',
-              padding: '10px',
-              fontSize: '18px',
-              fontWeight: '900',
-              cursor: 'pointer',
-              backgroundColor: offwhite,
-              color: black,
-              borderRadius: '10px',
-              border: 5,
-            }}
-          >
-            Generate
-          </button>
-        </div>
+        <button
+          onClick={handleGenerate}
+          style={{
+            width: '130px',
+            padding: '10px',
+            fontSize: '18px',
+            fontWeight: '900',
+            cursor: 'pointer',
+            backgroundColor: offwhite,
+            color: black,
+            borderRadius: '10px',
+            border: 5,
+          }}
+        >
+          Generate
+        </button>
       </div>
-    </>
+    </div>
   );
 }
